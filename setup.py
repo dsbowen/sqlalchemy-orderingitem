f3e30c5df013248b4149559e4a085e29375631fc
dsbowen <dsbowen@wharton.upenn.edu>
2019-11-19 08:25:55 -0500

    fixed typo in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sqlalchemy-ordering-item",
+    name="sqlalchemy-orderingitem",
     version="0.0.1",
     author="Dillon Bowen",
     author_email="dsbowen@wharton.upenn.edu",
